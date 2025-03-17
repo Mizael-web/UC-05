1027715e77f78704fa0615fe45ee7eadf5855c2c
MIZAEL MENDES DA SILVA <a95541100@rn.senac.local>
2025-03-17 17:29:26 -0300

    Add. routes na atividade

--- Aula_07_13_03_2025/src/modules/aluno/controllers/alunoControllers.js
+++ Aula_07_13_03_2025/src/modules/aluno/controllers/alunoControllers.js
@@ -1,90 +1,106 @@
-const AlunoModel = require ("../models/alunoModels")
+const AlunoModel = require("../models/alunoModels")
 
+// get =listar, post = criar, put=atualizar, delete = excluir
+class AlunoController {
 
-class alunoController {
-    
-       static async criar(requesicao, resposta){
+    static async criar(requesicao, resposta) {
         try {
 
-           const  { matricula, nome, email, senha} = requesicao.body
-           if (!matricula ||!nome ||!email ||!senha )
-           return resposta.status (200).json ({mensagem:" todos os campos devem ser preenchidos"})
-        
-           const novoAluno = await AlunoModel.criar(matricula, nome, email, senha)
-           resposta.status(201).json ({ mensagem: "Aluno criado com sucesso", aluno:novoAluno})        
-            
+            const { matricula, nome, email, senha } = requesicao.body
+            if (!matricula || !nome || !email || !senha)
+                return resposta.status(200).json({ mensagem: " todos os campos devem ser preenchidos" })
+
+            const novoAluno = await AlunoModel.criar(matricula, nome, email, senha)
+            resposta.status(201).json({ mensagem: "Aluno criado com sucesso", aluno: novoAluno })
+
         } catch (error) {
-          resposta.status(500).json ({mensagem: "Erro ao criar o aluno!, erro: error.message" })
-         }
-      }
-        
-      
-        static async editar(requisicao, resposta) {
-          try {
-              const matricula = requisicao.params.id;
-              const { nome, email, senha } = requisicao.body;
-              
-              const alunoAtualizado = await AlunoModel.editar(matricula, nome, email, senha);
-              if (!alunoAtualizado) {
-                  return resposta.status(400).json({ mensagem: "Aluno não encontrado !" });
-              }
-              resposta.status(200).json({ mensagem: "Aluno atualizado com sucesso", aluno: alunoAtualizado });
-          } catch (error) {
-              resposta.status(500).json({ mensagem: "Erro ao editar o aluno!", erro: error.message });
-          }
-      }
-     
-         
-        static async listarTodos(requisicao, resposta){
-                   
-       try {
-              const alunos = await AlunoModel.listar()
-              if(alunos.length === 0){
-                  return resposta.status(400).json({mensagem:"Não existe alunos a serem exibidos!"})
-              }
-              resposta.status(200).json(alunos)
-          } catch (error) {
-              resposta.status(500).json({mensagem:"Erro ao listar os alunos!", erro: error.message})
-          }
-      }
+            resposta.status(500).json({ mensagem: "Erro ao criar o aluno!, erro: error.message" })
+        }
+    }
 
-       static async listarPorMatricula(requesicao, resposta){
+
+    static async editar(requisicao, resposta) {
+        //http://localhost:3010/aluno/
         try {
-            const matricula = requesicao.params.id
-            const aluno = await AlunoModel.listarPorId(matricula)
-            if (!aluno){
-                return resposta.status(201).json ({menssagem: " Aluno não encontrado!"})
+            const matricula = requisicao.params.matricula;
+            const {nome, email, senha } = requisicao.body;
+
+            if (!nome || !email || !senha) {
+                return resposta.status(400).json(
+                    { mensagem: "todos os campos devem ser preenchidos!" });
             }
-                resposta.status(200).json (aluno)
-            
-        } catch (error) {
-            resposta.status(500).json ({mensagem: "Erro ao listar o aluno!, erro: error.message"})
-  
-         }
-      }
-        
 
-      static async excluirTodos(requisicao, resposta) {
+            const aluno = await AlunoModel.editar(matricula, nome, email, senha);
+            if (aluno.length === 0) {
+                return resposta.status(400).json(
+                    { mensagem: "Aluno não encontrado !" });
+            }
+            resposta.status(200).json(
+                {
+                    mensagem: "Aluno atualizado com sucesso", aluno: aluno
+                });
+        } catch (error) {
+            resposta.status(500).json(
+                {
+                    mensagem: "Erro ao editar o aluno!", erro: error.message
+
+                });
+        }
+    }
+
+
+    static async listarTodos(requisicao, resposta) {
+
         try {
-            const resultado = await AlunoModel.excluirTodos();
+            const alunos = await AlunoModel.listar()
+            if (alunos.length === 0) {
+                return resposta.status(400).json({ mensagem: "Não existe alunos a serem exibidos!" })
+            }
+            resposta.status(200).json(alunos)
+        } catch (error) {
+            resposta.status(500).json({ mensagem: "Erro ao listar os alunos!", erro: error.message })
+        }
+    }
+
+    static async listarPorMatricula(requesicao, resposta) {
+        try {
+            const matricula = requesicao.params.matricula
+            const aluno = await AlunoModel.listarPorMatriula(matricula)
+            if (aluno.length === 0) {
+                return resposta.status(201).json({ menssagem: " Aluno não encontrado!" })
+            }
+            resposta.status(200).json(aluno)
+
+        } catch (error) {
+            resposta.status(500).json({ mensagem: "Erro ao listar o aluno!, erro: error.message" })
+
+        }
+    }
+
+
+    static async excluirTodos(requisicao, resposta) {
+        try {
+
+            await AlunoModel.excluirTodos();
             resposta.status(200).json({ mensagem: "Todos os alunos foram excluídos com sucesso!", resultado });
         } catch (error) {
             resposta.status(500).json({ mensagem: "Erro ao excluir todos os alunos!", erro: error.message });
         }
     }
-      
+
     static async excluirPorMatricula(requisicao, resposta) {
-      try {
-          const matricula = requisicao.params.id;
-          const resultado = await AlunoModel.excluirPorID(matricula);
-          if (!resultado) {
-              return resposta.status(400).json({ mensagem: "Aluno não encontrado ou erro ao excluir!" });
-          }
-          resposta.status(200).json({ mensagem: "Aluno excluído com sucesso!" });
-      } catch (error) {
-          resposta.status(500).json({ mensagem: "Erro ao excluir o aluno!", erro: error.message });
-      }
-  }
+        try {
+            const matricula = requisicao.params.matricula;
+            const aluno = await AlunoModel.listarPorMatricula(matricula);
+            if (!aluno) {
+                return resposta.status(400).json({ mensagem: "Aluno não encontrado!" });
+            }
+            await AlunoModel.excluirPorMatricula(matricula)
+            resposta.status(200).json({ mensagem: "Aluno excluído com sucesso!" });
+        } catch (error) {
+            resposta.status(500).json({ mensagem: "Erro ao excluir o aluno!", erro: error.message });
+        }
+    }
 }
-     
-     module.exports = alunoController
+
+module.exports = AlunoController
--- Aula_07_13_03_2025/src/modules/aluno/models/alunoModels.js
+++ Aula_07_13_03_2025/src/modules/aluno/models/alunoModels.js
@@ -1,4 +1,4 @@
-const {pool}= require ("../../../modules/aluno")
+const {pool}= require ("../../../config/database")
 
 
 class  AlunoModel {
@@ -23,7 +23,7 @@ class  AlunoModel {
       return aluno.rows
 
    }
-   static async listarPorID(matricula){
+   static async listarPorMatricula(matricula){
     const dados = [matricula]
     const consulta =`select * from aluno where matricula = $1`
     const aluno = await pool.query(consulta, dados)
@@ -31,14 +31,13 @@ class  AlunoModel {
 
    }
 
-   static excluirPorID(matricula){
-   const dados = [matricula]
+   static async excluirPorMatricula(matricula){   
    const consulta = `delete * from aluno where matricula = $1`
    await pool.query(consulta, dados)
  
    }
 
-   static excluirTodos(){
+   static async excluirTodos(){
       const consulta = `delete *from aluno`
       await pool.query(consulta)
 
