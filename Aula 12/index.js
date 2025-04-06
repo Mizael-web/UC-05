26ac98e8f5b84981a893499c582c34cc259f5b5e
vlimap <valtemir.procopio@gmail.com>
2025-04-06 11:06:57 -0300

    feat: reestrutura aplicação para usar Express e modulariza rotas de aluno e endereço

--- Aula 12/index.js
+++ Aula 12/index.js
@@ -1,39 +1,20 @@
-const { pool } = require('./database');
+// Importando com (commonjs)
+const express = require("express")
+const dotenv = require("dotenv");
+const routerAluno = require('./src/modules/aluno/routes/index')
+const routerEndereco = require('./src/modules/endereco/routes/index')
 
-async function criarTabelas() {
-    try {
-        // Criar tabela aluno
-        await pool.query(`
-            CREATE TABLE IF NOT EXISTS aluno (
-                matricula SERIAL PRIMARY KEY,
-                nome VARCHAR(255) NOT NULL,
-                email VARCHAR(255) UNIQUE NOT NULL,
-                senha VARCHAR(255) NOT NULL
-            );
-        `);
+dotenv.config();
 
-        // Criar tabela endereco
-        await pool.query(`
-            CREATE TABLE IF NOT EXISTS endereco (
-                id SERIAL PRIMARY KEY,
-                matricula INTEGER NOT NULL REFERENCES aluno(matricula) ON DELETE CASCADE,
-                cep VARCHAR(10) NOT NULL,
-                logradouro VARCHAR(255) NOT NULL,
-                numero VARCHAR(10) NOT NULL,
-                complemento VARCHAR(255),
-                bairro VARCHAR(255) NOT NULL,
-                localidade VARCHAR(255) NOT NULL,
-                uf VARCHAR(2) NOT NULL,
-                ponto_referencia VARCHAR(255)
-            );
-        `);
+const port = process.env.PORTA;
+const app = express();
 
-        console.log('Tabelas criadas com sucesso!');
-    } catch (error) {
-        console.error('Erro ao criar tabelas:', error);
-    } finally {
-        pool.end();
-    }
-}
-// executar esse arquivo com 'node criar_tabela_com_pg.js'
-criarTabelas();
+// Aplicação use express como json(javascript object notation)
+app.use(express.json());
+
+app.use(routerAluno)
+app.use(routerEndereco)
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
